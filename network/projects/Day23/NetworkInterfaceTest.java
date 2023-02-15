00a15a8266f066c0988d7f1e86c0c3bcb1375e11
Cayley <cayley@dr.com>
2023-02-15 15:40:58 +0900

    day23 ,ãstringbuilder

--- network/projects/Day23/NetworkInterfaceTest.java
+++ network/projects/Day23/NetworkInterfaceTest.java
@@ -6,17 +6,38 @@ import java.util.Enumeration;
 import java.util.stream.Stream;
 
 public class NetworkInterfaceTest {
+    public static String getMACIdentifier(NetworkInterface network) {
+        StringBuilder identifier = new StringBuilder();
+        try {
+            byte[] macBuffer = network.getHardwareAddress();
+            if (macBuffer != null) {
+                for (int i = 0; i <
+                        macBuffer.length; i++) {
+                    identifier.append(
+                            String.format("%02X%s", macBuffer[i],
+                                    (i < macBuffer.length - 1)
+                                            ? "-" : ""));
+                }
+            } else {
+                return "---";
+            }
+        } catch (SocketException ex) {
+            ex.printStackTrace();
+        }
+        return identifier.toString();
+    }
+
     public static void main(String[] args) throws SocketException {
         Enumeration<NetworkInterface> interfaceEnum = NetworkInterface.getNetworkInterfaces();
 
         for (NetworkInterface element; interfaceEnum.hasMoreElements();) {
             element = interfaceEnum.nextElement();
+
             System.out.printf("%-8s %-32s\n", element.getName(), element.getDisplayName());
             Enumeration<InetAddress> addresses = element.getInetAddresses();
 
             for (InetAddress inetAddress : Collections.list(addresses))
                 System.out.printf(" InetAddress: %s\n", inetAddress);
-
         }
     }
 }
--- network/projects/Day23/Test.java
+++ network/projects/Day23/Test.java
@@ -1,3 +1,42 @@
-public enum Test {
-    a, b, c, d, e, f, g;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+
+class PrivateConstructor{
+    public static PrivateConstructor getNewInstance(){
+        return new PrivateConstructor();
+    }
+
+    private PrivateConstructor(){
+        System.out.println("make");
+    }
+}
+
+public class Test {
+    public static String getMAC(NetworkInterface network){
+        StringBuilder identifier = new StringBuilder();
+
+        try {
+            byte[] macBuffer = network.getHardwareAddress();
+            if (macBuffer != null) {
+                for (int i = 0; i <
+                        macBuffer.length; i++) {
+                    identifier.append(
+                            String.format("%02X%s",macBuffer[i],
+                                    (i < macBuffer.length - 1)
+                                            ? "-" : ""));
+                }
+            } else {
+                return "---";
+            }
+        } catch (SocketException ex) {
+            ex.printStackTrace();
+        }
+        return identifier.toString();
+    }
+
+    public static void main(String[] args) {
+        StringBuilder s = new StringBuilder("wow");
+        for (int i=0; ++i<=10;) s.append("!");
+        System.out.println(s);
+    }
 }
